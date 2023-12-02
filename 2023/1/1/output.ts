a4759357acd2d0e41e641d3d81ae74e1505e7566
Julian Krzysiak <jkrzysiak13@gmail.com>
2023-12-01 18:04:31 -0800

    Add correct implementation

--- 2023/1/1/output.ts
+++ 2023/1/1/output.ts
@@ -1,1 +1,20 @@
-console.log(123);
+const file = Bun.file(import.meta.dir + '/input.txt');
+const text = await file.text();
+const arr = text.split('\n');
+
+function getNumber(string: string): number {
+	const arr = Array.from(string);
+	const firstNumber = arr.find((x) => Number.isInteger(Number(x)));
+	const secondNumber = arr.findLast((x) => Number.isInteger(Number(x)));
+
+	if (typeof firstNumber !== 'string' || typeof secondNumber !== 'string')
+		throw new Error('You must have copied something wrong.');
+	return Number(firstNumber + secondNumber);
+}
+
+function addNumbers(arr: string[]) {
+	return arr.reduce((sum, element) => sum + getNumber(element), 0);
+}
+
+const result = addNumbers(arr);
+console.log(result);

29e6885b7264039c4e907677c5a81dba6ace06f9
Julian Krzysiak <jkrzysiak13@gmail.com>
2023-12-04 02:28:44 -0800

    Finish second part of day 4

--- 2023/04/output.ts
+++ 2023/04/output.ts
@@ -14,15 +14,36 @@ function parseText(arr: string) {
 
 function calculatePoints(arr: number[][]): number {
 	const [firstArr, secondArr] = arr;
-	const matches =
-		firstArr.flatMap((x) => (secondArr.includes(x) ? x : [])).length - 1;
-	const points = Math.floor(2 ** matches);
-	return points;
+	const matches = firstArr.flatMap((x) =>
+		secondArr.includes(x) ? x : []
+	).length;
+	return matches;
 }
 
 function addPoints(arr: number[][][]) {
-	return arr.reduce((sum, add) => sum + calculatePoints(add), 0);
+	const correctPoints = (arr: number[][]) =>
+		Math.floor(2 ** (calculatePoints(arr) - 1));
+	return arr.reduce((sum, add) => sum + correctPoints(add), 0);
 }
 const arr = parseText(text);
 const firstResult = addPoints(arr);
-console.log(firstResult);
+console.log('Part 1: ', firstResult);
+
+// Second Part //
+
+function recursive(arr: number[][][], index: number) {
+	let score = 1;
+	let points = calculatePoints(arr[index]);
+	if (!points) return 1;
+
+	const nextArr = Array.from({ length: points }, (_x, i) => index + i + 1);
+	nextArr.forEach((x) => (score += recursive(arr, x)));
+	return score;
+}
+
+function addScratchCards(arr: number[][][]) {
+	return arr.reduce((sum, _add, i) => sum + recursive(arr, i), 0);
+}
+
+const secondResult = addScratchCards(arr);
+console.log('Part 2: ', secondResult);

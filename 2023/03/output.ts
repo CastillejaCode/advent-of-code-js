43a2af7b0f6883ec553efaaced3d2e5f619e954c
Julian Krzysiak <jkrzysiak13@gmail.com>
2023-12-03 23:22:33 -0800

    Add function to check perimter

--- 2023/03/output.ts
+++ 2023/03/output.ts
@@ -1,7 +1,37 @@
 import text from './input.txt';
 const arr = text.split('\n');
 
-// Find number
+// Reverse each array
+function reverseArr(arr: string[]) {
+	return Array.from(arr, (x) => [...x].toReversed());
+}
+
+function getBox(coord: [number, number]) {
+	const [yCoord, xCoord] = coord;
+	const axes = [-1, 0, 1];
+
+	const box = [];
+	for (const y of axes) {
+		for (const x of axes) {
+			box.push([yCoord + y, xCoord + x]);
+		}
+	}
+	return box;
+}
+
+// Count around, check if symbol or number
+function countAround(arr: string[], coord: [number, number]) {
+	const box = getBox(coord);
+	const results = box.map(([y, x]) =>
+		!arr[y] || !arr[y][x] ? '.' : arr[y][x]
+	);
+	return {
+		numberNext: Number.isInteger(Number(results[5])),
+		symbol: results.some((e) => /[^\d.]/g.test(e)),
+	};
+}
+
+// If number, keep going and save state
 function findNumber(arr: string[]) {}
 
 // Get block around number
@@ -12,5 +42,4 @@ function findNumber(arr: string[]) {}
 
 //Add them up
 
-const firstResult = arr;
-console.log(firstResult[0].length);
+console.log(countAround(arr, [0, 1]));

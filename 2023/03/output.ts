a83b8bf050bf1a55efb9b079f9ba2fcc2e3ec0d8
Julian Krzysiak <jkrzysiak13@gmail.com>
2023-12-03 22:01:29 -0800

    Inital files for day 3

---
+++ 2023/03/output.ts
@@ -0,0 +1,16 @@
+import text from './input.txt';
+const arr = text.split('\n');
+
+// Find number
+function findNumber(arr: string[]) {}
+
+// Get block around number
+
+//Check for symbols
+
+//Do for every number
+
+//Add them up
+
+const firstResult = arr;
+console.log(firstResult[0].length);
